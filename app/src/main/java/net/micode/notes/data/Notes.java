f17759832a5c4157bc9a12edcd99b0350ab75d19
Hao <1538196177@qq.com>
2019-01-03 19:25:23 +0800

    标注：NoteItemData,NotesListItem

--- app/src/main/java/net/micode/notes/data/Notes.java
+++ app/src/main/java/net/micode/notes/data/Notes.java
@@ -75,7 +75,7 @@ public class Notes {
         public static final String PARENT_ID = "parent_id";
 
         /**
-         * Created data for note or folder
+         * Created date for note or folder
          * <P> Type: INTEGER (long) </P>
          */
         public static final String CREATED_DATE = "created_date";
